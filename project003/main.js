6cb5780e2c2fe623258582e826310113535ceb74
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-02-17 17:01:44 +0500

    typescript second project creatd

---
+++ project003/main.js
@@ -0,0 +1,2 @@
+var EinstienQuote = "Albert Einstien once said,A person who never made a mistake never tried anything new.";
+console.log(EinstienQuote);
---
+++ project003/main.ts
@@ -0,0 +1,2 @@
+let EinstienQuote:string ="Albert Einstien once said,A person who never made a mistake never tried anything new.";
+console.log(EinstienQuote);
