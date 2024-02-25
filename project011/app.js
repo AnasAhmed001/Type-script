501512db718162a4beca8ca10f4c11c39841ea21
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-02-25 16:29:39 +0500

    program12 completed

---
+++ project011/app.js
@@ -0,0 +1,11 @@
+//exercise10
+//let members:string[] =['anas','huzaifa','usman','usama'];
+//for(let i=0; i<members.length; i++){
+//  console.log(members[i]);
+//}
+//exercise 11
+var members = ['anas', 'huzaifa', 'usman', 'usama'];
+var message = 'tomorrow whose birthday ?:';
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ project011/app.ts
@@ -0,0 +1,14 @@
+//exercise10
+//let members:string[] =['anas','huzaifa','usman','usama'];
+//for(let i=0; i<members.length; i++){
+  //  console.log(members[i]);
+    
+//}
+
+//exercise 11
+let members:string[] =['anas','huzaifa','usman','usama'];
+let message:string='tomorrow whose birthday ?:';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+    
+}
