4252119273ffa24dd1d08922ffbd4e304a262366
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-02-25 15:02:32 +0500

    project 5 created

---
+++ project005/main.ts
@@ -0,0 +1,4 @@
+let quote: string= "A person who never made a mistake never tried anything new";
+let famousPerson: string= "Albert Einstien";
+let message=`${famousPerson} once said, ${quote}`;
+console.log(message);
