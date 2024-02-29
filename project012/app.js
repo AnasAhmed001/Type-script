56e137f258b271d1ee10b314815dc94927d16450
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-02-29 23:38:32 +0500

    project13 done

---
+++ project012/app.js
@@ -0,0 +1,4 @@
+var transportation = ['Honda', 'Bike', 'Civic', 'Suzuki'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('I would like to use a ' + transportation[i]);
+}
---
+++ project012/app.ts
@@ -0,0 +1,5 @@
+let transportation : string [] = ['Honda','Bike','Civic','Suzuki'];
+for(let i=0; i<transportation.length; i++){
+    console.log('I would like to use a ' +transportation[i]);
+    
+}
