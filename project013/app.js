235a545c0e7a88551b8329d83117286841fd0000
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-02-29 23:51:12 +0500

    project 13 completed

---
+++ project013/app.js
@@ -0,0 +1,4 @@
+var guest_List = ['Ali', 'Farhan', 'Usama', 'Kashif'];
+for (var i = 0; i < guest_List.length; i++) {
+    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow');
+}
---
+++ project013/app.ts
@@ -0,0 +1,4 @@
+let guest_List : string[] = ['Ali','Farhan','Usama','Kashif'];
+for(let i=0; i < guest_List.length; i++){
+    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow');
+}
