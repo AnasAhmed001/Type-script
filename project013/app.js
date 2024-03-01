f49b16b8ccdcf6c2a582a7adccfd00936c6a96f6
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-01 20:13:03 +0500

    project 15 also done in this folder

--- project013/app.js
+++ project013/app.js
@@ -1,7 +1,9 @@
+//project 13
 var guest_List = ['Ali', 'Farhan', 'Usama', 'Kashif'];
-for (var i = 0; i < guest_List.length; i++) {
-    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow\n');
-}
+// for(let i=0; i < guest_List.length; i++){
+//     console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+// }
+//project 14
 var not_present = 'Farhan';
 var new_guest = 'Imran Khan';
 guest_List[1] = new_guest;
@@ -9,3 +11,9 @@ for (var i = 0; i < guest_List.length; i++) {
     console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow\n');
 }
 console.log("Mr. ".concat(not_present, " will not arrive tomorrow for dinner"));
+//project 15
+guest_List.unshift('Shaheen', 'Usama', 'Rizwan');
+for (var i = 0; i < guest_List.length; i++) {
+    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow.We found a bigger dinner table.\n');
+}
+;
--- project013/app.ts
+++ project013/app.ts
@@ -1,8 +1,12 @@
-let guest_List : string[] = ['Ali','Farhan','Usama','Kashif'];
-for(let i=0; i < guest_List.length; i++){
-    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
-}
+//project 13
 
+let guest_List : string[] = ['Ali','Farhan','Usama','Kashif'];
+// for(let i=0; i < guest_List.length; i++){
+//     console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+// }
+
+
+//project 14
 let not_present : string ='Farhan';
 let new_guest : string ='Imran Khan';
 guest_List[1]= new_guest;
@@ -10,3 +14,10 @@ for(let i=0; i < guest_List.length; i++){
     console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
 }
 console.log(`Mr. ${not_present} will not arrive tomorrow for dinner`);
+
+//project 15
+
+guest_List.unshift('Shaheen','Usama','Rizwan');
+for(let i=0; i < guest_List.length; i++){
+    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow.We found a bigger dinner table.\n');
+};
