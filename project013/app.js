efa8a738fafce17abb1efdffe09166df5c954215
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-01 19:26:30 +0500

    project 13 and 14 done

--- project013/app.js
+++ project013/app.js
@@ -1,4 +1,11 @@
 var guest_List = ['Ali', 'Farhan', 'Usama', 'Kashif'];
 for (var i = 0; i < guest_List.length; i++) {
-    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow');
+    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow\n');
 }
+var not_present = 'Farhan';
+var new_guest = 'Imran Khan';
+guest_List[1] = new_guest;
+for (var i = 0; i < guest_List.length; i++) {
+    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+}
+console.log("Mr. ".concat(not_present, " will not arrive tomorrow for dinner"));
--- project013/app.ts
+++ project013/app.ts
@@ -1,4 +1,12 @@
 let guest_List : string[] = ['Ali','Farhan','Usama','Kashif'];
 for(let i=0; i < guest_List.length; i++){
-    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow');
+    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
 }
+
+let not_present : string ='Farhan';
+let new_guest : string ='Imran Khan';
+guest_List[1]= new_guest;
+for(let i=0; i < guest_List.length; i++){
+    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+}
+console.log(`Mr. ${not_present} will not arrive tomorrow for dinner`);
