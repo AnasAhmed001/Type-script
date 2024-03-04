d9565d049b2731fb695f18ce971508a9e85b5da5
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-04 23:45:29 +0500

    project 16 also complete in this folder

--- project013/app.js
+++ project013/app.js
@@ -7,13 +7,21 @@ var guest_List = ['Ali', 'Farhan', 'Usama', 'Kashif'];
 var not_present = 'Farhan';
 var new_guest = 'Imran Khan';
 guest_List[1] = new_guest;
-for (var i = 0; i < guest_List.length; i++) {
-    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow\n');
-}
+// for(let i=0; i < guest_List.length; i++){
+//     console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+// }
 console.log("Mr. ".concat(not_present, " will not arrive tomorrow for dinner"));
 //project 15
 guest_List.unshift('Shaheen', 'Usama', 'Rizwan');
-for (var i = 0; i < guest_List.length; i++) {
-    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow.We found a bigger dinner table.\n');
+// 
+// project 16
+console.log('\nUnfortunately we cannot arrange a big table, Only two people allow');
+while (guest_List.length > 2) {
+    var remove_guest = guest_List.pop();
+    console.log("Sorry Sir/Madam.".concat(remove_guest, " you are not invited for dinner"));
 }
-;
+for (var i = 0; i < guest_List.length; i++) {
+    console.log("Respected Sir/Madam " + guest_List[i] + ',\n Yes you are still invited tomorrow on dinner\n');
+}
+guest_List.splice(0, 2);
+console.log(guest_List);
--- project013/app.ts
+++ project013/app.ts
@@ -10,14 +10,28 @@ let guest_List : string[] = ['Ali','Farhan','Usama','Kashif'];
 let not_present : string ='Farhan';
 let new_guest : string ='Imran Khan';
 guest_List[1]= new_guest;
-for(let i=0; i < guest_List.length; i++){
-    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
-}
+// for(let i=0; i < guest_List.length; i++){
+//     console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+// }
 console.log(`Mr. ${not_present} will not arrive tomorrow for dinner`);
 
 //project 15
 
 guest_List.unshift('Shaheen','Usama','Rizwan');
-for(let i=0; i < guest_List.length; i++){
-    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow.We found a bigger dinner table.\n');
-};
+// 
+
+// project 16
+
+console.log('\nUnfortunately we cannot arrange a big table, Only two people allow');
+while(guest_List.length>2){
+    let remove_guest = guest_List.pop();
+    console.log(`Sorry Sir/Madam.${remove_guest} you are not invited for dinner`);
+    
+}    
+    for(let i=0; i < guest_List.length; i++){
+    console.log("Respected Sir/Madam " +guest_List[i] + ',\n Yes you are still invited tomorrow on dinner\n');
+}
+guest_List.splice(0,2)
+console.log(guest_List);
+
+    
