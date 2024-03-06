73ee66ef5e41e8b3a311f88ee09e1bd6f68d66d6
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-06 22:50:58 +0500

    project 17 created

---
+++ project017/seeingtheworld.ts
@@ -0,0 +1,28 @@
+let plces : string [] = ['cape town','delhi','faisalabad','karachi'];
+// console.log('original' + plces);
+//print your array in alphabatical order without modifiying the actual list.
+// console.log('copy' + [...plces].sort());
+
+//show that your array is in original order by printing it.
+
+// console.log('original' + plces);
+
+//show your array in reverse order without changing the original list.
+
+console.log('copy' + [...plces].sort().reverse());
+
+//show that your array is in original order by printing it again.
+
+console.log('copy' + [...plces].sort().reverse());
+
+//reverse the order of your list.Print the array to show that its order is changed.
+
+console.log('original' + plces.sort());
+
+//reverse the order of your list again in its original order.
+
+console.log('original' + plces.sort().reverse());
+
+
+
+
