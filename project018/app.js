c88e73b5877fc56e961864f1fb91cfc97485d9ec
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-06 23:13:49 +0500

    project 18 done

---
+++ project018/app.js
@@ -0,0 +1,6 @@
+var guest_List = ['Ali', 'Farhan', 'Usama', 'Kashif'];
+for (var i = 0; i < guest_List.length; i++) {
+    console.log("Respected Sir/Madam " + guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+}
+console.log('\n PRINTING NUMBER OF GUESTS INVITED');
+console.log("We have finally invited ".concat(guest_List.length, " from exercise 14"));
---
+++ project018/app.ts
@@ -0,0 +1,7 @@
+let guest_List : string[] = ['Ali','Farhan','Usama','Kashif'];
+for(let i=0; i < guest_List.length; i++){
+    console.log("Respected Sir/Madam " +guest_List[i] + ',\n We invited you on dinner tomorrow\n');
+}
+console.log('\n PRINTING NUMBER OF GUESTS INVITED');
+console.log(`We have finally invited ${guest_List.length} from exercise 14`);
+
