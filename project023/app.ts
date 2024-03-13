faf435920db688be737bdaf51914c73c98f44b44
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-13 23:41:55 +0500

    project 23 completed

---
+++ project023/app.ts
@@ -0,0 +1,46 @@
+let cars:string = 'Subaru';
+
+//Equality Expression True
+
+console.log('Is car == "Subaru"? I predict true.');
+console.log(cars == 'Subaru');
+
+//strict equality check
+
+console.log('Is car === "Subaru"? I predict true.');
+console.log(cars === 'Subaru');
+
+//inequality comparison
+
+console.log('Is car != "Subaru"? I predict false.');
+console.log(cars != 'Subaru');
+
+//strict inequality comparison
+
+console.log('Is car !== "Subaru"? I predict false.');
+console.log(cars !== 'Subaru');
+
+//less than comparison
+
+console.log('Is car < "Subaru"? I predict false.');
+console.log(cars < 'Subaru');
+
+//greater than comparison
+
+console.log('Is car > "Subaru"? I predict false.');
+console.log(cars > 'Subaru');
+
+//less than equal to comparison
+
+console.log('Is car <= "Subaru"? I predict true.');
+console.log(cars <= 'Subaru');
+
+//freater than equal to comparison
+
+console.log('Is car >= "Subaru"? I predict false.');
+console.log(cars >= 'Subaru');
+
+//checking truthness
+
+console.log('Is !car? I predict false.');
+console.log('!cars');
