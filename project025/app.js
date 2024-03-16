e771c3d8008d7b02f149f3ebba97201680e417e0
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-16 16:14:04 +0500

    project 25 done

---
+++ project025/app.js
@@ -0,0 +1,8 @@
+var alienColour = 'green';
+if (alienColour == "green") {
+    console.log("The player just earened five points!");
+}
+alienColour = "yellow";
+if (alienColour == "red") {
+    console.log("the player just eared five points");
+}
---
+++ project025/app.ts
@@ -0,0 +1,12 @@
+let alienColour:string = 'green';
+
+if(alienColour == "green"){
+    console.log("The player just earened five points!");
+    
+}
+
+alienColour="yellow";
+if(alienColour == "red"){
+    console.log("the player just eared five points");
+    
+}
