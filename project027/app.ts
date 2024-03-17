296de84b19242e8e1c10c389b1151942d75fd590
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-17 23:34:51 +0500

    project 27 completed

---
+++ project027/app.ts
@@ -0,0 +1,60 @@
+let alienColour_2 : string = "green";
+if(alienColour_2 == "green"){
+    console.log("Player just earned 5 points for shooting the alien");
+    
+}
+
+else if(alienColour_2 == "yellow"){
+    console.log("Player just earned 10 points for shooting the alien");
+    
+}
+
+
+else if(alienColour_2 == "red"){
+    console.log("Player just earned 15 points for shooting the alien");
+    
+}
+
+else{
+    console.log("please select colour!"); 
+}
+
+//version 2
+
+alienColour_2 = "red";
+
+if(alienColour_2 == "green"){
+    console.log("Player just earned 5 points for shooting the alien");
+    
+}
+
+else if(alienColour_2 == "yellow"){
+    console.log("Player just earned 10 points for shooting the alien");
+    
+}
+
+else if(alienColour_2 == "red"){
+    console.log("Player just earned 15 points for shooting the alien");
+    
+}
+
+//version 3
+
+alienColour_2 = "yellow";
+
+if(alienColour_2 == "green"){
+    console.log("Player just earned 5 points for shooting the alien");
+    
+}
+
+else if(alienColour_2 == "yellow"){
+    console.log("Player just earned 10 points for shooting the alien");
+    
+}
+
+else if(alienColour_2 == "red"){
+    console.log("Player just earned 15 points for shooting the alien");
+    
+}
+
+
