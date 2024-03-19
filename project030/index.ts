362d21ef695d8ad0c04ea5599587037b4809b10c
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-19 23:32:15 +0500

    project 30 done

---
+++ project030/index.ts
@@ -0,0 +1,13 @@
+let users: string[] = ['anas','usman','admin','tahoor'];
+
+for(let user of users){
+    if(user === 'admin'){
+        console.log("Hello admin, would you like to see a status report?");
+        
+    }
+    
+else{
+    console.log(`Hello ${user}, thankyou for logging in again!`);
+    
+}
+}
