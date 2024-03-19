dd2316584d86870d6510f5640961dc6d13f09fbc
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-19 23:19:38 +0500

    project 29 completed

---
+++ project029/index.js
@@ -0,0 +1,13 @@
+var favorite_fruits = ['apple', 'grapes', 'orange'];
+if (favorite_fruits.includes('apple')) {
+    console.log('You really like apple');
+}
+if (favorite_fruits.includes('orange')) {
+    console.log('You really like orange');
+}
+if (favorite_fruits.includes('grapes')) {
+    console.log('You really like grapes');
+}
+if (favorite_fruits.includes('bananas')) {
+    console.log('You really like bananas');
+}
