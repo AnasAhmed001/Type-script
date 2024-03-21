ece977c281cefdd85fa5c692b1f92f51732b9b7c
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-21 23:27:33 +0500

    project 31 done

---
+++ project031 no users/index.js
@@ -0,0 +1,8 @@
+var users = ['sana', 'usama', 'taha', 'admin', 'anas'];
+if (users.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    users = [];
+    console.log("All users have been removed" + users.length);
+}
---
+++ project031 no users/index.ts
@@ -0,0 +1,10 @@
+let users : string [] = ['sana','usama','taha','admin','anas'];
+if(users.length === 0){
+    console.log("We need to find some users!");
+    
+}
+else{
+    users = [];
+    console.log("All users have been removed" + users.length);
+    
+}
