8eadb2470ef322b56ebffc44e39460b9cbd2dab5
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-28 23:33:09 +0500

    project 34 done

---
+++ project0034/app.js
@@ -0,0 +1,8 @@
+var myPizza = ["cheese pizza", "pepproni pizza", "veguterian pizza"];
+for (var i = 0; i < myPizza.length; i++) {
+    console.log(myPizza[i]);
+}
+for (var i = 0; i < myPizza.length; i++) {
+    console.log("I like to eat ".concat(myPizza[i]));
+}
+console.log("I like to eat pizza . Pizza comes in various toppings and flavours , allowing individual to customize it.");
---
+++ project0034/app.ts
@@ -0,0 +1,11 @@
+let myPizza = ["cheese pizza", "pepproni pizza","veguterian pizza"];
+for(let i=0; i <myPizza.length; i++){
+    console.log(myPizza[i]);
+}
+
+for(let i=0; i < myPizza.length; i++){
+    console.log(`I like to eat ${myPizza[i]}`);
+    
+}
+
+console.log("I like to eat pizza . Pizza comes in various toppings and flavours , allowing individual to customize it.");
