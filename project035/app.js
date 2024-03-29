c7b1bb3bb9e6119dd4a50628cc76712df55a9cac
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-29 23:27:18 +0500

    project 35 completed

---
+++ project035/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["dog", "cat", "rabbit"];
+animals.forEach(animal => {
+    console.log(`A ${animal} would be a great pet`);
+});
+console.log("Any of these animals can be a great pet");
---
+++ project035/app.ts
@@ -0,0 +1,8 @@
+let animals = ["dog", "cat", "rabbit"];
+animals.forEach(animal => {
+    console.log(`A ${animal} would be a great pet`);
+});
+console.log("Any of these animals can be a great pet");
+
+    
+    
