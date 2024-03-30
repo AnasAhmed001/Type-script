ce77a5c4856e751fb9ba92195026fe4640da3c11
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-03-30 23:41:39 +0500

    project39 done

---
+++ project039/app.js
@@ -0,0 +1,7 @@
+function city_country(nameofcity, country) {
+    console.log("".concat(nameofcity, " , ").concat(country));
+}
+city_country("Karachi", "Pakistan");
+console.log(city_country("UAE", "Dubai"));
+console.log(city_country("London", "United Kingdom"));
+console.log(city_country("Riyaz", "Saudi Arabia"));
---
+++ project039/app.ts
@@ -0,0 +1,10 @@
+function city_country(nameofcity: string , country: string){
+    console.log(`${nameofcity} , ${country}`);
+    
+}
+city_country("Karachi" , "Pakistan");
+
+console.log(city_country("UAE" , "Dubai"));
+console.log(city_country("London", "United Kingdom"));
+console.log(city_country("Riyaz" , "Saudi Arabia"));
+
