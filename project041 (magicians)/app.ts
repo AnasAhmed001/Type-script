7c5cac4341e4e46c925026fde66b96ab37fc1086
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-04-01 01:32:59 +0500

    project 41 done

---
+++ project041 (magicians)/app.ts
@@ -0,0 +1,15 @@
+let magician:string[] = ['Harry Potter','Hermione Granger','Ron Weasly','Albus Dumbledore'];
+
+
+
+
+function show_magicians(magicians:string[]){
+    magicians.forEach(element => {
+        console.log(element);
+        
+    });
+}
+
+
+show_magicians(magician);
+
