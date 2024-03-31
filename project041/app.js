ba4a120aef76cc5c99958b556e3c555919b990f9
AnasAhmed001 <153764760+AnasAhmed001@users.noreply.github.com>
2024-04-01 01:34:09 +0500

    Delete project041 directory

--- project041/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let magician = ['Harry Potter', 'Hermione Granger', 'Ron Weasly', 'Albus Dumbledore'];
-function show_magicians(magicians) {
-    magicians.forEach(element => {
-        console.log(element);
-    });
-}
-show_magicians(magician);
