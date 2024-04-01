8e1858ef5f07d6e4dfdd842dd56a4c490d725d91
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-04-01 23:06:21 +0500

    project 43 done

---
+++ project043/app.ts
@@ -0,0 +1,39 @@
+let magician:string[] = ['Harry Potter','Hermione Granger','Ron Weasly','Albus Dumbledore'];
+
+function copy_array(arr:string[]){
+    return[...arr]
+}
+
+
+
+
+
+
+function make_great(magicianArray:string[]){
+    for(let i = 0; i<magicianArray.length; i++){
+        magicianArray[i] = 'the great '+ magicianArray[i]
+    }
+
+}
+
+
+function show_magicians(magicians:string[]){
+    magicians.forEach(element => {
+        console.log(element);
+        
+    });
+}
+
+
+const copymagicianarray =  copy_array(magician)
+
+
+make_great(copymagicianarray);
+
+console.log('\n\nThis is my original array');
+
+show_magicians(magician);
+
+console.log('\n\nThis is my modified copy of the array');
+
+show_magicians(copymagicianarray);
